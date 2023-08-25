c7b707025e55e99616383c250f7807105ee8fee0
safderun <burakberkkeskin@gmail.com>
2023-08-26 00:18:36 +0300

    added p10k install

--- lib/ohmyzshConfiguration.sh
+++ lib/ohmyzshConfiguration.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
+zshrcPath="${HOME}/.zshrc"
+
 updatePluginsListZshrc(){
   # Define the new list of plugins to add
-  new_plugins=("$@")
-
-  # Define the file path
-  file_path="${HOME}/.zshrc"
+  newPlugins=("$@")
 
   # Read the current plugins line from the file
-  current_plugins=$(grep -oP 'plugins=\(\K[^)]+' "$file_path")
+  currentPlugins=$(grep -oP 'plugins=\(\K[^)]+' "$zshrcPath")
 
   # Convert the current plugins to an array
-  IFS=' ' read -ra current_plugins_array <<< "$current_plugins"
+  IFS=' ' read -ra current_plugins_array <<< "$currentPlugins"
 
   # Merge the current plugins and new plugins while removing duplicates
-  mapfile -t merged_plugins < <(printf "%s\n" "${current_plugins_array[@]}" "${new_plugins[@]}" | sort -u)
+  mapfile -t mergedPlugins < <(printf "%s\n" "${current_plugins_array[@]}" "${newPlugins[@]}" | sort -u)
 
   # Generate the new plugins line
-  new_plugins_string="plugins=(${merged_plugins[*]})"
+  newPluginsString="plugins=(${mergedPlugins[*]})"
 
   # Use sed to replace the old plugins line with the new one
-  sed -i "s/plugins=(.*)/$new_plugins_string/" "$file_path"
+  sed -i "s/plugins=(.*)/$newPluginsString/" "$zshrcPath"
+}
 
+updateZshTheme(){
+  newThemeName=$1
+
+  # Use sed to replace the old ZSH_THEME line with the new one
+  sed -i "s/ZSH_THEME=\"[^\"]*\"/ZSH_THEME=\"$newThemeName\"/" "$zshrcPath"
+  cp ./configurations/p10k.conf "${HOME}/.p10k.zsh"
 }
 
 installZshSyntaxHighligting(){
   info "Installing zsh syntax highligting"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  rm -rf "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
   updatePluginsListZshrc "zsh-syntax-highlighting"
 }
 
 installZshAutoSuggestions(){
   info "Installing zsh auto suggestions"
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  rm -rf "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
   updatePluginsListZshrc "zsh-autosuggestions"
+}
+
+installPowerlevel10k(){
+  info "Installing powerlevel10k theme"
+  rm -rf "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+  updateZshTheme "powerlevel10k/powerlevel10k"
 }
 
 configureOhMyZsh(){
