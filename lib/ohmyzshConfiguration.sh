df3ba52addb9d0c5e4f59a5ade4b2cfc54fe2ef9
safderun <burakberkkeskin@gmail.com>
2023-08-26 00:26:51 +0300

    added escape character to theme name

--- lib/ohmyzshConfiguration.sh
+++ lib/ohmyzshConfiguration.sh
@@ -26,7 +26,7 @@ updateZshTheme(){
   newThemeName=$1
 
   # Use sed to replace the old ZSH_THEME line with the new one
-  sed -i "s@ZSH_THEME=\"[^\"]*\"@ZSH_THEME=\"$newThemeName\"@" "$zshrcPath"
+  sed -i "s/ZSH_THEME=\"[^\"]*\"/ZSH_THEME=\"$newThemeName\"/" "${zshrcPath}"
   cp ./configurations/p10k.conf "${HOME}/.p10k.zsh"
 }
 
@@ -48,7 +48,7 @@ installPowerlevel10k(){
   info "Installing powerlevel10k theme"
   rm -rf "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-  updateZshTheme "powerlevel10k/powerlevel10k"
+  updateZshTheme "powerlevel10k\/powerlevel10k"
 }
 
 configureOhMyZsh(){
