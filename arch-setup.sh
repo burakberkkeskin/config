37bba9ed61440665910625b1aa1cd0e0fb4f077e
Burak Berk Keskin <berk@Buraks-MacBook-Pro.local>
2023-08-25 14:56:27 +0300

    added install with pacman

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,7 +4,7 @@ source lib/log.sh
 source lib/systemUpdate.sh
 source lib/gitSetup.sh
 source lib/yaySetup.sh
-source lib/installPackageWithYay.sh
+source lib/installPackage.sh
 source lib/detectDistro.sh
 
 main(){
@@ -15,20 +15,23 @@ main(){
   checkGit || installGit
   checkYay || installYay
 
-  local testPackages=("wireguard-tools" "zenv" "docker" "docker-compose")
+  local testYayPackages=("wireguard-tools" "zenv" "docker" "docker-compose")
 
-  local clientPackages=("brave-bin" "discord_arch_electron" "spotify" "mailspring" 
+  local clientYayPackages=("brave-bin" "discord_arch_electron" "spotify" "mailspring" 
       "flameshot" "kazam" "qbittorrent" "btop"
       "openvpn3" "wireguard-tools" "terminator"  
       "zsh")
 
-  local developerPackages=("code" "vim" "dbeaver" 
+  local developerYayPackages=("code" "vim" "dbeaver" 
         "docker" "docker-buildx" "docker-compose" 
         "kubectl" "act-bin" "ansible" "dive" 
         "aws-cli-v2" "postman-bin" "trivy" 
         "zenv" "xdman" "terraform")
-  
-  installPackageWithYay "${testPackages[@]}"
+
+  local developerPacmanPackages=("nodejs" "npm" "python" "go")
+
+  installPackageWithYay "${testYayPackages[@]}"
+  installPackageWithPacman "${developerPacmanPackages[@]}"
 }
 
 main
--- lib/installPackageWithYay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-installPackageWithYay(){
-  title "Installing packages with yay"
-  yay -S "${@}" --noconfirm 
-}
